fc3e79d304051dfc6a086a1b1fd586443734d31c
Andrew Wooldridge <triptych@gmail.com>
2021-12-04 13:07:11 -0800

    first commit

---
+++ src/components/Footer.svelte
@@ -0,0 +1,18 @@
+<footer>
+    <hr/>
+    <ul>
+        <li><a href="mailto:triptych@gmail.com">email me</a></li>
+    </ul>
+    <p>Copyright &copy; Andrew Wooldridge 2021</p>
+</footer>
+<style>
+    ul {
+        display: flex;
+        font-size:small;
+        list-style-type: none;
+    }
+    p {
+        text-align: center;
+        font-size: smaller;
+    }
+</style>
